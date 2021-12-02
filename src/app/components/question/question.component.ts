ecc5ea79c72cabdf156f3510dc28077623bd9b29
Neil Smith <s3nt1n3lz18@gmail.com>
2021-12-02 14:26:18 +0000

    Added question component

---
+++ src/app/components/question/question.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Question } from 'src/app/app.component';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
+})
+export class QuestionComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() question: Question;
+
+  ngOnInit(): void {}
+
+}
