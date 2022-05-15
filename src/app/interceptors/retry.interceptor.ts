498a3a4f790f26cc0180f79ce70e9e92158fa998
Neil Smith <s3nt1n3lz18@gmail.com>
2022-05-15 12:54:45 +0100

    Added retry interceptor

---
+++ src/app/interceptors/retry.interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
+
+const RETRY_COUNT = 2;
+
+@Injectable()
+export class RetryInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      retry(RETRY_COUNT),
+    )
+  }
+}
