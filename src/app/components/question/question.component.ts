7cf80ede5ad4d25069a2c42cef36bbea10cbdfe8
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2024-09-04 11:42:31 +0100

    Adding ability to add and remove tags

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -4,6 +4,8 @@ import { Store } from '@ngrx/store';
 import { AppStateWrapper } from 'src/app/store/reducer';
 import { Router } from '@angular/router';
 import { SetEditingQuestion, SetSelectedQuestion } from '../../store/action';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 @Component({
 	selector: 'app-question',
@@ -12,26 +14,59 @@ import { SetEditingQuestion, SetSelectedQuestion } from '../../store/action';
 })
 export class QuestionComponent implements OnInit {
 
+	// Define the separator key codes for adding tags
+	readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+    
 	constructor(
-    private store: Store<AppStateWrapper>,
-    private router: Router
+        private store: Store<AppStateWrapper>,
+        private router: Router
 	) {}
 
-  @Input() question: Question;
-  @Input() showAnswer = false;
-  @Output() showAnswerChange = new EventEmitter<boolean>();
+    @Input() question: Question;
+    @Input() showAnswer = false;
+    @Output() showAnswerChange = new EventEmitter<boolean>();
 
-  ngOnInit(): void {}
+    ngOnInit(): void {}
 
-  revealAnswer = () => {
-  	this.showAnswer = !this.showAnswer;
-  	this.showAnswerChange.emit(this.showAnswer);
-  };
+    // Toggle the display of the answer
+    revealAnswer = () => {
+    	this.showAnswer = !this.showAnswer;
+    	this.showAnswerChange.emit(this.showAnswer);
+    };
 
-  edit = () => {
-  	// Set the selected question
-  	this.store.dispatch(new SetSelectedQuestion(this.question));
-  	this.store.dispatch(new SetEditingQuestion(true));
-  	this.router.navigate(['add-question']);
-  };
+    // Navigate to the edit page
+    edit = () => {
+    	this.store.dispatch(new SetSelectedQuestion(this.question));
+    	this.store.dispatch(new SetEditingQuestion(true));
+    	this.router.navigate(['add-question']);
+    };
+
+    // Add a new tag
+    add(event: MatChipInputEvent): void {
+		console.log('adding tag: ', event);
+    	const input = event.input;
+    	const value = event.value.trim();
+
+    	// Add the tag if it's not empty and doesn't already exist
+    	if (value && !this.question.tags.includes(value)) {
+    		this.question.tags.push(value);
+    		// Optionally, update the store or perform other actions here
+    	}
+
+    	// Reset the input value
+    	if (input) {
+    		input.value = '';
+    	}
+    }
+
+    // Remove an existing tag
+    remove(tag: string): void {
+		console.log('removing tag: ', tag);
+    	const index = this.question.tags.indexOf(tag);
+
+    	if (index >= 0) {
+    		this.question.tags.splice(index, 1);
+    		// Optionally, update the store or perform other actions here
+    	}
+    }
 }
