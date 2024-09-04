495f3b6c8af121cee537a2669e04e7ac2d891ae7
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2024-09-04 09:51:28 +0100

    Updated button styles

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 @NgModule({
 	exports: [
+		MatButtonModule,
 		MatChipsModule,
 		MatInputModule,
 		MatAutocompleteModule,
+		MatFormFieldModule,
 		MatIconModule,
 	]
 })
