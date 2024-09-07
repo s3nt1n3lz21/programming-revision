49bc36e7dd3f441b0fc1de7660f775ccc20efb90
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2024-09-07 19:52:46 +0100

    Added logging to revision page component

--- src/app/pages/revision/revision.component.ts
+++ src/app/pages/revision/revision.component.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { BarChartData } from 'src/app/model/IBarChart';
-import { Question, emptyQuestion, DAY } from 'src/app/model/IQuestion';
-import { ApiService } from 'src/app/services/api.service';
-import { UpdateQuestion } from 'src/app/store/action';
+import { Question, emptyQuestion } from 'src/app/model/IQuestion';
+import { LoggerService, LogLevel } from 'src/app/services/logger.service';
 import { AppStateWrapper } from 'src/app/store/reducer';
-import { EventEmitter } from 'stream';
 
 @Component({
 	selector: 'app-revision',
@@ -22,51 +19,56 @@ export class RevisionComponent implements OnInit {
 
 	// App State
 	questionsStore: Observable<Question[]>;
-	questions: Question[];
+	questions: Question[] = [];
 
 	constructor(
-    private store: Store<AppStateWrapper>,
+		private store: Store<AppStateWrapper>,
+		private logger: LoggerService // Injecting LoggerService
 	) { 
 		this.questionsStore = this.store.select(state => state.state.questions);
+		this.logger.log(LogLevel.INFO, 'RevisionComponent', 'Component created');
 	}
 
 	ngOnInit(): void {
+		this.logger.log(LogLevel.INFO, 'RevisionComponent', 'ngOnInit called');
 		this.questionsStore.subscribe(
 			(questions) => {
 				this.questions = questions;
+				this.logger.log(LogLevel.DEBUG, 'RevisionComponent', 'Questions loaded', questions);
 				this.nextQuestion();
 			},
-			(error) => { console.error(error); }
+			(error) => { 
+				this.logger.log(LogLevel.ERROR, 'RevisionComponent', 'Error loading questions', error); 
+			}
 		);
 	}
 
-	// There are questions with answers that we haven't answered
 	public questionsLeft() {
-		return this.questions.some((q) => q.answer != '' && new Date(q.answerExpiryDate) < new Date());
+		const hasQuestionsLeft = this.questions.some((q) => q.answer != '' && new Date(q.answerExpiryDate) < new Date());
+		this.logger.log(LogLevel.DEBUG, 'RevisionComponent', 'Checking questions left', hasQuestionsLeft);
+		return hasQuestionsLeft;
 	}
 
-	public selectQuestion = (question: Question) => {
-		// if (new Date(question.answerExpiryDate) < new Date()) {
+	public selectQuestion(question: Question) {
+		this.logger.log(LogLevel.INFO, 'RevisionComponent', 'Selecting question', question);
 		this.showCurrentAnswer = false;
 		this.index = this.questions.findIndex(q => q.id === question.id);
 		this.currentQuestion = question;
-		// }
-	};
-  
+	}
+
 	public nextQuestion() {
+		this.logger.log(LogLevel.INFO, 'RevisionComponent', 'Next question requested');
 		let randomIndex = 0;
 
 		if (this.questionsLeft()) {
 			let answeredThisQuestion = true;
 			let limit = 0;
-			// Find the questions i've answered the least
 			const timesAnsweredCorrectly = this.questions.filter(x => new Date(x.answerExpiryDate) < new Date() && x.question && x.answer).map(x => x.timesAnsweredCorrectly);
 			const leastTimesAnsweredCorrectly = Math.min(...timesAnsweredCorrectly);
 			const questionsAnsweredTheLeast = this.questions.filter(x => x.timesAnsweredCorrectly == leastTimesAnsweredCorrectly);
 
-			// Get a random question that we haven't answered
 			while (answeredThisQuestion && limit < 10000) {
-				randomIndex = Math.floor(Math.random()*questionsAnsweredTheLeast.length);
+				randomIndex = Math.floor(Math.random() * questionsAnsweredTheLeast.length);
 				if ((new Date(questionsAnsweredTheLeast[randomIndex].answerExpiryDate) < new Date()) && questionsAnsweredTheLeast[randomIndex].answer) {
 					answeredThisQuestion = false;
 				}
@@ -74,8 +76,10 @@ export class RevisionComponent implements OnInit {
 			}
     
 			this.index = this.questions.findIndex(x => x.id == questionsAnsweredTheLeast[randomIndex].id);
+			this.logger.log(LogLevel.DEBUG, 'RevisionComponent', 'Next question selected', this.questions[this.index]);
 		} else {
 			this.index = 0;
+			this.logger.log(LogLevel.INFO, 'RevisionComponent', 'No questions left');
 		}
 
 		this.showCurrentAnswer = false;
@@ -83,15 +87,19 @@ export class RevisionComponent implements OnInit {
 	}
 
 	trackByFn(index: number, item: Question) {
-		// console.log('item.id:', item.id);
+		this.logger.log(LogLevel.DEBUG, 'RevisionComponent', 'Tracking question by id', item.id);
 		return item.id;
 	}
 
 	public sum() {
-		return this.questions.map(q => q.timesAnsweredCorrectly).reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+		const total = this.questions.map(q => q.timesAnsweredCorrectly).reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+		this.logger.log(LogLevel.INFO, 'RevisionComponent', 'Sum of correct answers', total);
+		return total;
 	}
 
 	public noQuestionsLeft() {
-		return this.questions.map(q => new Date(q.answerExpiryDate) < new Date() ).reduce((previousValue, currentValue) => previousValue + Number(currentValue), 0);
+		const noQuestions = this.questions.map(q => new Date(q.answerExpiryDate) < new Date() ).reduce((previousValue, currentValue) => previousValue + Number(currentValue), 0);
+		this.logger.log(LogLevel.INFO, 'RevisionComponent', 'No questions left', noQuestions);
+		return noQuestions;
 	}
 }
